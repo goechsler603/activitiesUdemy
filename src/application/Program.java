b2642a84cfa798e4958b2a9890baae1842e2d945
goechsler <oechslergui@gmail.com>
2025-05-03 19:11:24 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,59 @@
+package application;
+
+import entities.BankAccount;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Lets create your account:");
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Enter your account number:");
+        int accountNumber = sc.nextInt();
+        BankAccount account1 = new BankAccount(name, accountNumber);
+
+        while (true) {
+            String answer;
+            System.out.print("Do you want to deposit? Y/N: ");
+            answer = sc.nextLine().toUpperCase();
+
+            if (answer.equals("Y")) {
+                System.out.print("Enter your first deposit amount: ");
+                double firstDeposit = sc.nextDouble();
+                account1.deposit(firstDeposit);
+                break;
+            } else if (answer.equals("N")) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter 'Y' or 'N'.");
+            }
+        }
+
+
+        System.out.println("Account data");
+        System.out.println(account1.toString());
+
+        System.out.println("\nHow much do you want to deposit?");
+        double amount = sc.nextDouble();
+        account1.deposit(amount);
+        System.out.println(account1.toString());
+
+        System.out.println("\nHow much do you want to withdraw?");
+        double draft = sc.nextDouble();
+        account1.bankDraft(draft);
+        System.out.println(account1.toString());
+
+        System.out.println("Updated account data");
+        System.out.println(account1.toString());
+
+    }
+}
---
+++ src/entities/BankAccount.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class BankAccount {
+
+    public String accountName;
+    private final int accountNumber;
+    private double accountBalance;
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public BankAccount(String accountName, int accountNumber) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+    }
+
+    public void bankDraft (double draft){
+        accountBalance = accountBalance - draft - 5;
+    }
+
+    public void deposit(double amount){
+        accountBalance += amount;
+    }
+
+    public String toString() {
+        return  "Account:"
+                + accountNumber
+                + ", Holder:  "
+                + accountName
+                + ", Account Balance: $ "
+                + String.format("%.2f", accountBalance);
+    }
+}
