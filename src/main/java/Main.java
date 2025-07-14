eed33a74be2df8b25e3f12f456600811ca045919
goechsler <oechslergui@gmail.com>
2025-07-14 20:23:57 -0300

    Employee class created

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("How many employees?: ");
+
+
+    }
+}
