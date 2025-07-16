12a9e8435c0fda8ae1c9a9f566bc951a6f6799ac
goechsler <oechslergui@gmail.com>
2025-07-15 21:05:46 -0300

    Finished the main code

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+
 
 public class Employee {
+
+    private Long id;
+    private String name;
+    private double salary;
+
+    public Employee(Long id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void increaseSalary(double percent) {
+        double aux = (percent * 0.01) * salary;
+        salary += aux;
+    }
+
+    public String toString() {
+        return id + ", " + name + ", " + String.format("%.2f", salary);
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
-package com;
-
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,8 +9,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        System.out.println("How many employees?: ");
 
+        System.out.println("How many employees?: ");
+        int n = sc.nextInt();
+
+        List<Employee> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Emplyee #" + (i + 1) + ":");
+            System.out.print("Id: ");
+            Long id = sc.nextLong();
+            sc.nextLine();
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Salary: ");
+            double salary = sc.nextDouble();
+            Employee employee = new Employee(id, name, salary);
+            list.add(employee);
+        }
+
+        System.out.println();
+        System.out.println("Enter the employee that will have salary increase: ");
+        int idIncrease = sc.nextInt();
+
+        Employee emp = list.stream().filter(x -> x.getId() == idIncrease).findFirst().orElse(null);
+
+        if (emp != null) {
+            System.out.println("Enter the porcentage:");
+            double porcentage = sc.nextDouble();
+            emp.increaseSalary(porcentage);
+        }else {
+            System.out.println("Employee not found");
+        }
+
+        System.out.println();
+        System.out.println("List of employees: ");
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
 
     }
 }
